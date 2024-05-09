649a7db47b448ab6bfcddca42ec1bd31034d165a
Lucas Peixoto <lucaspeixoto349@gmail.com>
2024-05-09 19:25:36 -0300

    refatorando JobController

--- src/main/java/com/peixoto/gestao_vagas/security/SecurityFilter.java
+++ src/main/java/com/peixoto/gestao_vagas/security/SecurityFilter.java
@@ -1,9 +1,14 @@
 package com.peixoto.gestao_vagas.security;
 
 import java.io.IOException;
+import java.util.Collections;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import com.peixoto.gestao_vagas.providers.JWTProvider;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -11,15 +16,34 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 @Component
-public class SecurityFilter extends OncePerRequestFilter{
+public class SecurityFilter extends OncePerRequestFilter {
 
-    @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-            throws ServletException, IOException {
+  @Autowired
+  private JWTProvider jwtProvider;
 
-                String header = request.getHeader("Authorization");
-                System.out.println(header);
-        filterChain.doFilter(request, response);
+  @Override
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+      throws ServletException, IOException {
+    SecurityContextHolder.getContext().setAuthentication(null);
+    String header = request.getHeader("Authorization");
+
+    System.out.println(header);
+
+    if (header != null) {
+      var subjectToken = this.jwtProvider.validateToken(header);
+      if (subjectToken.isEmpty()) {
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        return;
+      }
+
+      request.setAttribute("company_id", subjectToken);
+      UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(subjectToken, null,
+          Collections.emptyList());
+
+      SecurityContextHolder.getContext().setAuthentication(auth);
     }
-    
+
+    filterChain.doFilter(request, response);
+  }
+
 }
