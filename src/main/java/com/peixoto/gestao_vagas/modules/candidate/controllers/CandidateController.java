1cef5ec2f34e8e46a94e3743827b27d2e7d28388
Lucas Peixoto <lucaspeixoto349@gmail.com>
2024-05-12 12:44:39 -0300

    Adicionando Swagger no Controller

--- src/main/java/com/peixoto/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/com/peixoto/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -19,6 +19,13 @@ import com.peixoto.gestao_vagas.modules.candidate.useCases.ListAllJobsByFilterUs
 import com.peixoto.gestao_vagas.modules.candidate.useCases.ProfileCandidateUseCase;
 import com.peixoto.gestao_vagas.modules.company.entities.JobEntity;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 
@@ -62,6 +69,15 @@ public class CandidateController {
 
     @GetMapping("/job")
     @PreAuthorize("hasRole('CANDIDATE')")
+    @Tag(name = "Candidato", description = "Informações do candidato")
+    @Operation(summary = "Listagem de vagas disponíveis para o candidato", description = "Função responsável por listar todas as vagas disponíveis baseada no filtro")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", content = {
+            @Content(
+                array = @ArraySchema(schema = @Schema(implementation = JobEntity.class))
+            )
+        })
+    })
     public List<JobEntity> findJobByFilter(@RequestParam String filter){
         return this.listAllJobsByFilterUseCase.execute(filter);
     }
